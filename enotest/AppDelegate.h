4a6c056d89f44f648526e4e738c9f8577cbf679c
adam <adam@adamdeMacBook-Pro.local>
2017-07-18 15:10:03 +0800

    Initial Commit

---
+++ enotest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  enotest
+//
+//  Created by adam on 2017/7/18.
+//  Copyright © 2017年 adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ enotest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  enotest
+//
+//  Created by adam on 2017/7/18.
+//  Copyright © 2017年 adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
